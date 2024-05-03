173b3702c18ce8470d2d4b4811f60c563e3aad7b
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-05-04 05:50:24 +0700

    format serta menginstall react-router-dom

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
-import React from "react"
+import React from "react";
 
-export default  function Card() {
-    return(
-        <div className="shadow-lg bg-white rounded-lg p-7 hover:-translate-y-2 shadow-xl">
-        <h1 className="font-bold text-lg">samsung a12</h1>
-        <h2 className="text-sm text-gray-600">12.000.000</h2>
-        </div>
-    )
+export default function Card() {
+  return (
+    <div className="bg-white rounded-lg p-7 hover:-translate-y-2 shadow-xl">
+      <h1 className="font-bold text-lg">samsung a12</h1>
+      <h2 className="text-sm text-gray-600">12.000.000</h2>
+    </div>
+  );
 }
